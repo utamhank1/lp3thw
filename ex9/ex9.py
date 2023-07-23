8b40c6ebb5c6eaf9e3779945e22622faf53f721e
Ujjwal Tamhankar <ujjwaltamhankar@Ujjwals-MacBook-Air-2.local>
2023-07-23 19:04:32 -0400

    Exercise 9 files

---
+++ ex9/ex9.py
@@ -0,0 +1,19 @@
+def main():
+	# Here is some new strange stuff, remember type it exactly.
+
+	days = "Mon Tues Wed Thurs Fri Sat Sun"
+	months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
+
+	print("Here are the days: ", days)
+	print("Here are the months: ", months)
+
+	print("""
+		There's something going on here.
+		With the three double-quotes.
+		We'll be able to type as much as we like.
+		Even 4 lines if we want, or 5 or 6.
+		""")
+
+
+if __name__ == "__main__":
+	main()
